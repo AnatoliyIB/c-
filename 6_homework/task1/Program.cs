af62c6fe81e4e342cf4d8f8f893f45ca61782e7e
Anatoliy <anatoliy1983@tut.by>
2024-02-02 21:38:17 +0300

    CreateMatrix_and_ShowMatrix

---
+++ 6_homework/task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+
+
+char [,] CreateMatrix(int rowCount, int columsCount) // Функция создания матрицы
+{char[,] matrix = new char[rowCount, columsCount];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = (char)rnd.Next(97, 123);
+}
+}
+return matrix;
+}
+
+void ShowMatrix(char[,] matrix) // Функция вывода матрицы на экран
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write($"{matrix[i, j]} ");
+}
+Console.WriteLine();
+}
+}
+
+char[,] matrix = CreateMatrix(3, 4);
+ShowMatrix(matrix);
+
+
+
