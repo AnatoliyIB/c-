0e4b23b8ef1eb77ed4ab1c1c418b0236a52c3e9d
Anatoliy <anatoliy1983@tut.by>
2024-02-02 16:02:38 +0300

    L2_T2

---
+++ 2_lecture/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a=int.Parse(Console.ReadLine()!);
+int b=int.Parse(Console.ReadLine()!);
+if (b*b==a)
+{
+    Console.WriteLine("True");
+}
+else 
+{
+    Console.WriteLine("Foulse");
+}
