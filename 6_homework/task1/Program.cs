4c08724e64de15e028cf2ce1308b493b9d33610a
Anatoliy <anatoliy1983@tut.by>
2024-02-02 22:00:33 +0300

    Task1_complited

--- 6_homework/task1/Program.cs
+++ 6_homework/task1/Program.cs
@@ -31,5 +31,16 @@ Console.WriteLine();
 char[,] matrix = CreateMatrix(3, 4);
 ShowMatrix(matrix);
 
+void ConvertArrayToString(char[,] matrix) // Функция вывода матрицы на экран
+{ string str="";
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+str = str + matrix[i,j];
+}
+}
+Console.WriteLine(str);
+}
 
-
+ConvertArrayToString(matrix);
