7b0da98c613d28f63576b257799bb6eeb7a97212
Anatoliy <anatoliy1983@tut.by>
2024-02-03 15:53:16 +0300

    Replace str

---
+++ 6_homework/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string str = Console.ReadLine()!;
+string str2 ="";
+for  (int i=0; i<str.Length; i++)
+{
+    str2=str2+ str[str.Length-1-i];
+}
+Console.WriteLine(str2);
