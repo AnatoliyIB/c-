530c0381dc37e7229f33ba1ad8cb425fd3a7f07e
Anatoliy <anatoliy1983@tut.by>
2024-02-03 16:09:34 +0300

    T3_Complited

--- 6_homework/Task3/Program.cs
+++ 6_homework/Task3/Program.cs
@@ -1,9 +1,23 @@
 ﻿//Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
 
-string str = Console.ReadLine()!;
+string CreateReplaceStr(string str)
+{
 string str2 ="";
 for  (int i=0; i<str.Length; i++)
 {
     str2=str2+ str[str.Length-1-i];
 }
-Console.WriteLine(str2);
+return str2;
+}
+
+void IfPolindrom (string str, string str2)
+{if (str == str2)
+{
+    Console.WriteLine("Polindrom");
+}
+else Console.WriteLine("Not Polindrom");
+}
+
+string str=Console.ReadLine();
+string str2=CreateReplaceStr(str);
+IfPolindrom(str, str2);
