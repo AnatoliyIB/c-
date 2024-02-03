62b7b817a6924f5cd0102b8c812e624044c50bb4
Anatoliy <anatoliy1983@tut.by>
2024-02-03 16:38:21 +0300

    T4_strToStrArray

---
+++ 6_homework/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+//Сформировать строку, в которой слова расположены в обратном порядке. 
+//В полученной строке слова должны быть также разделены пробелами.
+
+Console.WriteLine("Введите строку, состоящую из слов, разделенных пробелами");
+string str = Console.ReadLine()!;
+char [] delimiter = {' '};
+string [] strArray = str.Split(delimiter,StringSplitOptions.None);
+foreach (string s in strArray) 
+{
+    Console.WriteLine(s);
+}
