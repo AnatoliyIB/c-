b91aca85a3a1eee8bb054f5079250cf4f0b84d88
Anatoliy <anatoliy1983@tut.by>
2024-02-04 16:08:15 +0300

    SumRows

---
+++ 5_homework/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] SumRows(int[,] array)
+    {
+      int [] sumrow= new int [array.GetLength(0)];
+     int sum=0;
+      //Напишите свое решение здесь
+      for (int i=0; i<array.GetLength(0); i++)
+      {
+        sum=0;
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+          sum=sum+array[i,j];
+          sumrow[i]=sum;
+        }
+        
+      }
+      return sumrow;
+    }
+    
+    void PrintArray (int [] array)
+    {
+        for (int i=0; i<array.Length; i++)
+        {
+            Console.Write(array[i]+ " ");
+        }
+    }
+    
+int[,] numbers = new int[,] {
+    {1, 2, 3},
+    {1, 1, 0},
+    {7, 8, 2},
+    {9, 10, 11}
+};
+int [] result=SumRows(numbers);
+PrintArray(result);
