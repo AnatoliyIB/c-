edb27746654769cca019a964f11a24c7925f08fc
Anatoliy <anatoliy1983@tut.by>
2024-02-05 21:50:33 +0300

    Task2_2_Finished

---
+++ 7_homework/Task2_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 2.2: Задайте значения M и N. Напишите программу,
+//которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии
+
+void PrintNumbersBetween(int M, int N)
+{
+    if (M == N - 1)
+    {
+        return;
+    }
+    if (M % 2 == 1)
+    {
+        Console.WriteLine(M + 1);
+    }
+    else
+    {
+        Console.WriteLine(M + 2);
+    }
+
+    PrintNumbersBetween(M + 2, N);
+}
+
+PrintNumbersBetween(1, 10);
