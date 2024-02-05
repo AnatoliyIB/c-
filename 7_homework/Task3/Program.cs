e3930beda2d8ec3782e6fd93b3804b6d2066045f
Anatoliy <anatoliy1983@tut.by>
2024-02-05 22:18:46 +0300

    Task3_Solved

---
+++ 7_homework/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
+//Использовать рекурсию, не использовать циклы.
+
+void ReverseArray(int[] array, int i)
+{
+    if (i == array.Length)
+    {
+        return;
+    }
+    Console.Write($"{array[array.Length - 1 - i]} ");
+    ReverseArray(array, i + 1);
+}
+int[] array = { 1, 2, 3, 4, 5 };
+ReverseArray(array, 0);
