c4bb25fa5caf908af91a894eb22b31e6b8a99712
Anatoliy <anatoliy1983@tut.by>
2024-02-05 21:14:11 +0300

    Task1 solved

---
+++ 7_homework/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу,
+//которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы
+
+void PrintNumbersBetween (int M, int N)
+{
+    if (M==N-1)
+    {
+        return;
+        }
+    Console.WriteLine(M+1);
+    PrintNumbersBetween (M+1,N);
+    
+
+     
+}
+
+PrintNumbersBetween(1,10);
